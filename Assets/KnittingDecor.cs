41b3a40faea813d74b48070cd380fe239b6e5f92
pl0nch1-ops <54858409+pl0nch1@users.noreply.github.com>
2021-06-07 01:34:44 +0300

    Звуки, двери, вязание и т.д.

---
+++ Assets/KnittingDecor.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnittingDecor : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    private void FixedUpdate()
+    {
+        transform.Rotate(new Vector3(0, _speed, 0));
+    }
+}
